9371bebf4f8193b7a8c0dc30fc72185544c7f3fb
Sandhya0824 <sandhyaa246789@gmail.com>
2023-09-11 03:56:29 +0530

    committed prototype and singleton

---
+++ SingletonPattern/VehiclePrototypePattern/AudiBuilder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehiclePrototypePattern
+{
+    public class AudiBuilder : IVehicle
+    {
+        public string Engine { get; set; }
+        public string VehicleNo { get; set; }
+
+        public IVehicle Clone()
+        {
+            Console.WriteLine("Audi is Cloned");
+            return (IVehicle)MemberwiseClone();
+        }
+
+        public void Initialize()
+        {
+            Console.WriteLine("Audi is Initialized");
+        }
+    }
+}
---
+++ SingletonPattern/VehiclePrototypePattern/VehicleManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehiclePrototypePattern
+{
+    public class VehicleManager
+    {
+        BMWBuilder bmw;
+        AudiBuilder audi;
+
+        public VehicleManager()
+        {
+            bmw = new BMWBuilder();
+            bmw.Initialize();
+
+            audi = new AudiBuilder();
+            audi.Initialize();
+        }
+
+        public IVehicle CreateAudi()
+        {
+            return audi.Clone();
+        }
+
+        public IVehicle CreateBMW()
+        {
+            return bmw.Clone();
+        }
+    }
+}
