00034674185c63c8488138f7d4d80888309f8aca
Sandhya0824 <sandhyaa246789@gmail.com>
2023-09-11 11:12:56 +0530

    updated Adapter Pattern

---
+++ DesignPatterns/AdapterPattern/PngImageAdapter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPattern
+{
+    public class PngImageAdapter : IImage
+    {
+        private PngImage pngImage;
+
+        public PngImageAdapter(PngImage image)
+        {
+            pngImage = image;
+        }
+
+        public void DisplayImage()
+        {
+            pngImage.ShowPngImage();
+        }
+
+    }
+}
---
+++ DesignPatterns/AdapterPattern/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace AdapterPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IImage jpegImage = new JpegImage();
+            IImage pngImage = new PngImageAdapter(new PngImage());
+
+            jpegImage.DisplayImage();
+
+            pngImage.DisplayImage();
+        }
+    }
+}
